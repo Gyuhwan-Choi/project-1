4bea4de9bea9534d81e25ef790052e1f9604873a
Gyuhwan <112284092+Choi0619@users.noreply.github.com>
2023-03-03 01:08:24 -0600

    add index, css, and javascript file for the project 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function changeBackground() {
+  
+  document.querySelectorAll("*").forEach(el => el.style.color = "white");
+  document.querySelectorAll("h1").forEach(el => el.style.color = "yellow");
+  document.querySelectorAll("h2").forEach(el => el.style.color = "orange");
+  document.querySelectorAll("label").forEach(el => el.style.color = "#dfdfdf");
+  document.querySelectorAll(".select-bigger").forEach(el => el.style.color = "#aaaaaa");
+  
+  if (document.body.style.backgroundImage === 'url("media/background2.jpg")') {
+    location.reload();
+  }
+  
+  var body = document.getElementsByTagName("body")[0];
+
+  body.style.backgroundImage = "url('media/background2.jpg')";
+}
